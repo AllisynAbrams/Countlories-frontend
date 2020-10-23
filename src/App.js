d54346fc7b69a7ce043d7ed0ad32c299e0e08ecc
Jake Nguyen <jakey@Jakes-Air.fios-router.home>
2020-10-23 13:22:30 -0400

    bootstrap navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import NavBar from './NavBar/NavBar'
 
 function App() {
   return (
     <div className="App">
-      Countlories
+      <NavBar />
     </div>
   );
 }
