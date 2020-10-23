214be59aae3551fde781938540f0ec8766065911
Lien Nguyen <lien@Liens-MacBook-Air.local>
2020-10-23 13:08:05 -0700

    about me component is done

--- src/About/About.js
+++ src/About/About.js
@@ -4,6 +4,8 @@ const About = () => {
 
     return (
         <div className='About'>
+            <h1>What is Countlories?</h1>
+            <p>Countlories is an all purpose food/calorie tracker. This response web app help user track and count their daily food and calorie consumption by displaying food/calories and total calories by day. It incorporates a form which allows a user to add a new food and its calories to a given day, as well as edit a food. As a user adds new foods, the daily total calorie intake will increase. This app can be used to track foods/calories as consumed or as a weekly food/calorie planner.</p>
 
         </div>
     )
