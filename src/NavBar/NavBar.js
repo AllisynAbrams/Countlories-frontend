96757dfa222ed0994a1bb6bb84c3040225fa9b23
Jake Nguyen <jakey@Jakes-Air.fios-router.home>
2020-10-23 14:24:18 -0400

    navbar somewhat styled

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import {Navbar, Nav} from 'react-bootstrap'
+import './navbar.scss'
 
 const NavBar = () => {
 	return (
 		<>
-			<Navbar bg='light' expand='lg' sticky='top'>
-				<Navbar.Brand href='/'>Countlories</Navbar.Brand>
+			<Navbar className='color-nav' expand='lg' sticky='top'>
+				<Navbar.Brand href='/'>
+					<img src='https://i.imgur.com/mL0La8x.png' alt='brand logo' />
+				</Navbar.Brand>
 				<Navbar.Toggle aria-controls='basic-navbar-nav' />
 				<Navbar.Collapse id='basic-navbar-nav'>
 					<Nav className='mr-auto'>
-						<Nav.Link href='/'>Home</Nav.Link>
-						<Nav.Link href='/about'>About</Nav.Link>
-						<Nav.Link href='/team'>Team</Nav.Link>
+						<Nav.Link className='link' href='/'>
+							Home
+						</Nav.Link>
+						<Nav.Link className='link' href='/about'>
+							About
+						</Nav.Link>
+						<Nav.Link className='link' href='/team'>
+							Team
+						</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Navbar>
