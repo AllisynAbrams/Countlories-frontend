a9d1125169422575a31ab3ceec3c1eac8e0416a8
Allisyn Abrams <allisynabrams@Allisyns-MBP-2.fios-router.home>
2020-10-23 17:29:18 -0400

    added days state, getDays, useEffect, and passed state to Days comp

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Days from '../Days/Days'
 
 const Home = () => {
+    const url = 'https://countlories.herokuapp.com'
 
+    const [days, setDays] = useState([])
 
+    const getDays = () => {
+		fetch(url)
+			.then((res) => res.json())
+			.then((data) => {
+                console.log('this is data', data)
+				setDays(data.data);
+			});
+	};
+
+	useEffect(() => {
+		getDays();
+    }, []);
+    
     return(
         <div className="Home">
-            <Days />
+            
+            <Days days={days}/>
+            
         </div>
     )
 }
