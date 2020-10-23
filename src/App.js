2a3bb56416d5ff3d7cbabcce8109441d4bc7aabf
Jake Nguyen <jakey@Jakes-Air.fios-router.home>
2020-10-23 14:57:35 -0400

    teams layout and html changes for Countlories

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import {Switch, Route} from 'react-router-dom'
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavBar from './NavBar/NavBar'
+import Team from './Team/Team'
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-    </div>
-  );
+		<div className='App'>
+			<NavBar />
+			<Switch>
+				<Route exact path='/' />
+				<Route exact path='/team'>
+					<Team />
+				</Route>
+				<Route exact path='/about'>
+					
+				</Route>
+			</Switch>
+		</div>
+	)
 }
 
 export default App;
