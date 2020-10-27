3bb963650322085aeebc2db66f824d9976c86d9e
Allisyn Abrams <allisynabrams@Allisyns-MBP-2.fios-router.home>
2020-10-27 15:06:00 -0400

    navbar scss updated

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { AnimatePresence, motion } from 'framer-motion'
 
-const Modal = ({ isToggled, setToggle, children }) => {
+const Modal = ({ isToggled, setToggle, setDayToggle, children }) => {
 	return (
 		<AnimatePresence>
 			{isToggled && (
@@ -10,15 +10,22 @@ const Modal = ({ isToggled, setToggle, children }) => {
 						initial={{ opacity: 0 }}
 						animate={{ opacity: 1 }}
 						exit={{ opacity: 0 }}
-                        style={{
-                            position: "fixed",
-                            top: "200px",
-                            width: "75%",
-                            left: "50%",
-                            transform: "translate3d(-50%, 0,0)"
-                        }}>
+						style={{
+							position: 'fixed',
+							top: '200px',
+							width: '75%',
+							left: '50%',
+							transform: 'translate3d(-50%, 0,0)',
+						}}>
 						<div className='form'>
-							<p className='form-x' onClick={() => setToggle(false)}>X</p>
+							<p
+								className='form-x'
+								onClick={() => {
+									setDayToggle(true)
+									setToggle(false)
+								}}>
+								X
+							</p>
 							{children}
 						</div>
 					</motion.div>
