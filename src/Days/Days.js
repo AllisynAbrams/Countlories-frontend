cf5f744a67e53870e40ca73c531c0e57b352869f
Jake Nguyen <jakey@Jakes-Air.fios-router.home>
2020-10-27 11:00:28 -0400

    bug fix attempt

--- src/Days/Days.js
+++ src/Days/Days.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react'
-import { Route, Link, Switch } from 'react-router-dom'
-import Form from '../Form/Form'
+// import { Route, Link, Switch } from 'react-router-dom'
+// import Form from '../Form/Form'
 import './days.scss'
+import Modal from '../Modal/Modal'
+import { motion } from 'framer-motion'
 
 const Days = (props) => {
 	// console.log('this is props', props)
+	console.log('day id', props.days)
 	const url = 'https://countlories.herokuapp.com'
 	const emptyFood = {
 		foodItem: String,
@@ -14,6 +17,7 @@ const Days = (props) => {
 
 	const [food, setFood] = useState([])
 	const [selectedFood, setSelectedFood] = useState(emptyFood)
+	const [isToggled, setToggle] = useState(false)
 
 	const getFoods = () => {
 		fetch(url + '/food/')
@@ -28,13 +32,13 @@ const Days = (props) => {
 		getFoods()
 	}, [])
 
-	const handleCreate = (day, food) => {
-		fetch(url + day._id, {
+	const handleCreate = (newFood) => {
+		fetch(url + '/food/', {
 			method: 'post',
 			headers: {
 				'Content-Type': 'application/json',
 			},
-			body: JSON.stringify(food),
+			body: JSON.stringify(newFood),
 		}).then(() => getFoods())
 	}
 
@@ -58,6 +62,18 @@ const Days = (props) => {
 		setSelectedFood(food)
 	}
 
+	const [formData, setFormData] = useState()
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		handleCreate(formData)
+		// props.history.push('/')
+	}
+
+	const handleChange = (e) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value })
+	}
+
 	let displayDays = <h1>Loading...</h1>
 	if (props.days[0]) {
 		displayDays = props.days.map((days) => {
@@ -65,12 +81,12 @@ const Days = (props) => {
 				<div className='day' key={days._id}>
 					<div className='day-header'>
 						<p>{days.day}</p>
-						<p>test date</p>
+						<input type='date' />
 						<h4>{days.date}</h4>
 					</div>
-					<Link to='/create'>
-						<p className='add'>+</p>
-					</Link>
+					<p className='add' onClick={() => setToggle(true)}>
+						+
+					</p>
 					<div className='day-body'>
 						<p className='total'>Total</p>
 						<p className='total-amount'>###</p>
@@ -82,7 +98,9 @@ const Days = (props) => {
 									<p className='foods'>{food.foodItem}</p>
 									<p className='calories'>{food.calories}</p>
 									<p className='edit'>Edit</p>
-									<p className='x' onClick={() => deleteFood(food)}>X</p>
+									<p className='x' onClick={() => deleteFood(food)}>
+										X
+									</p>
 								</>
 							)
 						})}
@@ -92,7 +110,50 @@ const Days = (props) => {
 		})
 	}
 
-	return <div className='Days'>{displayDays}</div>
+	return (
+		<motion.div
+			className='Days'
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			transition={{ duration: 1 }}>
+			<Modal isToggled={isToggled} setToggle={setToggle}>
+				<div className='form'>
+					<form onSubmit={handleSubmit} food={{emptyFood}}>
+						<p>What did you eat?</p>
+						<input
+							type='text'
+							name='Food Item'
+							// value={formData.foodItem}
+							onChange={handleChange}
+							placeholder='Food Item'
+						/>
+						<p>How many calories?</p>
+						<input
+							type='number'
+							name='Calories'
+							// value={formData.calories}
+							onChange={handleChange}
+							placeholder='Calories'
+						/>
+						<p>What time did you eat this?</p>
+						<input
+							type='text'
+							name='Time'
+							// value={formData.time}
+							onChange={handleChange}
+							placeholder='Time (eg: 2:00pm)'
+						/>
+						<input
+							className='countlories-btn'
+							type='submit'
+							value='COUNTLORIES'
+						/>
+					</form>
+				</div>
+			</Modal>
+			{displayDays}
+		</motion.div>
+	)
 }
 
 export default Days
