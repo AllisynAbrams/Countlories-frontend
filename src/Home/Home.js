d2a4be1457cc7d81ea1ee32c3ae91f5b4a3e76de
Jake Nguyen <jakey@Jakes-Air.fios-router.home>
2020-10-28 15:20:06 -0400

    delete and add buttons for home

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Days from '../Days/Days'
-import { Route } from 'react-router-dom';
+import { Route } from 'react-router-dom'
+import './home.scss'
 
 const Home = () => {
 	const url = 'https://countlories.herokuapp.com'
@@ -12,20 +13,47 @@ const Home = () => {
 			.then((res) => res.json())
 			.then((data) => {
 				// console.log('this is data', data.data)
-				setDays(data.data) 
+				setDays(data.data)
 			})
 	}
-
 
 	useEffect(() => {
 		getDays()
 	}, [])
 
-	console.log('this is days', days);
+	console.log('this is days', days)
+
+	const deleteDays = () => {
+		fetch(url, {
+			method: 'delete',
+		}).then(() => getDays())
+	}
+
+	const createDays = (day) => {
+		fetch(url + '/seed', {
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(day),
+		}).then(() => getDays())
+	}
 
 	return (
 		<div className='Home'>
-			<Route exact path='/' render={(rp) => ( <Days {...rp} days={days} getDays={getDays}/>)} />
+			<div className='home-buttons'>
+				<p className='delete-week' onClick={() => deleteDays()}>
+					Delete This Week
+				</p>
+				<p className='new-week' onClick={() => createDays()}>
+					Add New Week
+				</p>
+			</div>
+			<Route
+				exact
+				path='/'
+				render={(rp) => <Days {...rp} days={days} getDays={getDays} />}
+			/>
 		</div>
 	)
 }
