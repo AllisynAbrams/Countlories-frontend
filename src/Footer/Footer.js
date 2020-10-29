2f2703c7ef17faffa0f9a5460a40ff228cf0c2ba
Allisyn Abrams <allisynabrams@Allisyns-MBP-2.fios-router.home>
2020-10-29 13:54:07 -0400

    footer added

---
+++ src/Footer/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './footer.scss'
+
+const Footer = () => {
+    return <p className='footer'>{'\u00A9'} 2020 Countlories All Rights Reserved</p>;
+}
+
+export default Footer
